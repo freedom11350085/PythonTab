fc986867e0a223bcf8708b9fa3221badd333e253
freedom11350085 <freedom11350085@gmail.com>
2016-06-22 20:24:30 +0800

    basic

--- tab.py
+++ tab.py
@@ -1,16 +1,7 @@
 import sys
 import readline
 import rlcompleter
-import atexit
 import os
+readline.parse_and_bind('tab:  complete')
+histfile = os.path.join(os.environ['HOME'],  '.pythonhistory')
 
-readline.parse_and_bind('tab: complete')
-
-histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
-try:
-    readline.read_history_file(histfile)
-except IOError:
-    pass
-atexit.register(readline.write_history_file, histfile)
-
-del os, histfile, readline, rlcompleter
